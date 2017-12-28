ddcc721eb2a917bfbe7bd04151b088e6920c60e2
Murray <378516602@qq.com>
2017-12-28 18:06:59 +0800

    类的高级操作

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -12,7 +12,10 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
     }
 }
 
+
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27(this, other)
+operator fun MyDate.plus(timeInterval: TimeInterval) = addTimeIntervals(timeInterval, 1)
+operator fun MyDate.plus(timeInterval: RepeatedTimeInterval) = addTimeIntervals(timeInterval.timeInterval, timeInterval.number)
 
 enum class TimeInterval {
     DAY,
@@ -20,7 +23,22 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+operator fun TimeInterval.times(number: Int) = RepeatedTimeInterval(this, number)
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val number: Int)
+
+
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterator<MyDate> {
+    var current = start
+    override fun next(): MyDate {
+        val result = current
+        current = current.nextDay()
+        return result
+    }
+
+    override fun hasNext(): Boolean {
+        return current <= endInclusive
+    }
+}
 
 operator fun DateRange.contains(date: MyDate): Boolean = checkRangIn(this, date)
 
