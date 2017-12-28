0b4d507cd615aeaee7abf8dca5da17276786b915
Murray <378516602@qq.com>
2017-12-28 17:05:50 +0800

    实现 > < >= <= =操作符 实现 rangeTo .. 操作 实现 in 操作

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,18 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.util.*
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return when {
+            other.year != year -> year - other.year
+            other.month != month -> month - other.month
+            else -> dayOfMonth - other.dayOfMonth
+        }
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -11,3 +21,18 @@ enum class TimeInterval {
 }
 
 class DateRange(val start: MyDate, val endInclusive: MyDate)
+
+operator fun DateRange.contains(date: MyDate): Boolean = checkRangIn(this, date)
+
+fun checkRangIn(dateRange: DateRange, date: MyDate): Boolean {
+    val a = Calendar.getInstance()
+    a.set(date.year, date.month, date.dayOfMonth)
+
+    val b = Calendar.getInstance()
+    b.set(dateRange.start.year, dateRange.start.month, dateRange.start.dayOfMonth)
+
+    val c = Calendar.getInstance()
+    c.set(dateRange.endInclusive.year, dateRange.endInclusive.month, dateRange.endInclusive.dayOfMonth)
+
+    return a in b..c
+}
--- src/iii_conventions/n27RangeTo.kt
+++ src/iii_conventions/n27RangeTo.kt
@@ -2,19 +2,33 @@ package iii_conventions
 
 import util.TODO
 import util.doc26
+import java.util.*
 
-fun todoTask27(): Nothing = TODO(
-    """
-        Task 27.
-        Uncomment the commented code and make it compile.
-        Make all the changes to the file MyDate.kt.
+/**
+Task 27.
+Uncomment the commented code and make it compile.
+Make all the changes to the file MyDate.kt.
 
-        Tips: To make '..' work implement a 'MyDate.rangeTo()' extension function returning 'DateRange'.
-    """,
-    documentation = doc26()
-)
+Tips: To make '..' work implement a 'MyDate.rangeTo()' extension function returning 'DateRange'.
+ */
+fun todoTask27(first: MyDate, last: MyDate): DateRange {
+    return DateRange(first, last)
+}
 
 fun checkInRange2(date: MyDate, first: MyDate, last: MyDate): Boolean {
-    todoTask27()
-//    return date in first..last
+    //return checkUseCalendar(date, first, last)
+    return date in first..last
+}
+
+fun checkUseCalendar(date: MyDate, first: MyDate, last: MyDate): Boolean {
+    val a = Calendar.getInstance()
+    a.set(date.year, date.month, date.dayOfMonth)
+
+    val b = Calendar.getInstance()
+    b.set(first.year, first.month, first.dayOfMonth)
+
+    val c = Calendar.getInstance()
+    c.set(last.year, last.month, last.dayOfMonth)
+
+    return a in b..c
 }
