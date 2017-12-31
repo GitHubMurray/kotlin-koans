5a8be8a348d372fc0bd5a207ba846b8c3609f9e9
Murray <378516602@qq.com>
2017-12-31 20:37:44 +0800

    fix

--- src/v_builders/n39HtmlBuilders.kt
+++ src/v_builders/n39HtmlBuilders.kt
@@ -6,23 +6,23 @@ import v_builders.data.getProducts
 import v_builders.htmlLibrary.*
 
 fun getTitleColor() = "#b9c9fe"
-fun getCellColor(row: Int, column: Int) = if ((row + column) %2 == 0) "#dce4ff" else "#eff2ff"
+fun getCellColor(row: Int, column: Int) = if ((row + column) % 2 == 0) "#dce4ff" else "#eff2ff"
 
 fun todoTask39(): Nothing = TODO(
-    """
+        """
         Task 39.
         1) Fill the table with the proper values from products.
         2) Color the table like a chess board (using getTitleColor() and getCellColor() functions above).
         Pass a color as an argument to functions 'tr', 'td'.
         You can call the 'main' function in the 'htmlDemo.kt' to see the rendered table.
     """,
-    documentation = doc39()
+        documentation = doc39()
 )
 
 fun renderProductTable(): String {
-    return html {
+    val html = html {
         table {
-            tr {
+            tr(getTitleColor()) {
                 td {
                     text("Product")
                 }
@@ -33,8 +33,38 @@ fun renderProductTable(): String {
                     text("Popularity")
                 }
             }
+
             val products = getProducts()
-            todoTask39()
+//            for (index in products.indices) {
+//                tr {
+//                    td {
+//                        text(products[index].description)
+//                    }.set("bgcolor", getCellColor(index, 0))
+//                    td {
+//                        text(products[index].price)
+//                    }.set("bgcolor", getCellColor(index, 1))
+//                    td {
+//                        text(products[index].popularity)
+//                    }.set("bgcolor", getCellColor(index, 2))
+//                }
+//            }
+
+            for ((index, product) in products.withIndex()) {
+                tr {
+                    td(getCellColor(index, 0)) {
+                        text(product.description)
+                    }
+                    td(getCellColor(index, 1)) {
+                        text(product.price)
+                    }
+                    td(getCellColor(index, 2)) {
+                        text(product.popularity)
+                    }
+                }
+            }
         }
     }.toString()
+
+    println(html)
+    return html
 }
